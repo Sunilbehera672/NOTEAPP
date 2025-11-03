cbd06ba91b339d1d7a8f3de4c915f506a546f037
Sunilbehra672 <sunilkumar671234567@gmail.com>
2025-11-03 17:59:35 +0530

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@
 
     export async function connectToDb(){
         try {
-            await mongoose.connect( "mongodb://127.0.0.1:27017/NoteApp");
+            await mongoose.connect(process.env.MONGO_URI);
             
         } catch (error) {
             console.log(error)
